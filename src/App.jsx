df6e48989328c9a992bbb84262d716647651a7f4
alireza-parvali-76 <alirezaparvali76@gmail.com>
2025-11-06 11:40:14 +0330

    Design Header

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Components/Header'
+ 
+import './App.css'
+
+export default function App() {
+  return (
+    <div className='flex items-center justify-center min-h-dvh bg-gray-600 h-full'>
+     <div className=''>
+      <Header />
+     </div>
+    </div>
+  )
+}
---
+++ src/Components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { GiNotebook } from "react-icons/gi";
+
+
+export default function Header() {
+  return (
+    <div className=''>
+      <div className='bg-white w-140 p-4 rounded-2xl'>        
+          <div className='flex items-center justify-center'>
+            <span><GiNotebook className="text-2xl text-blue-400"/></span>
+            <span className='text-black font-serif text-2xl'>یادداشت های من</span>
+          </div>
+          <div className='flex items-center justify-center mt-4'>
+            <span>0</span>
+            <span className='font-serif font-medium text-[18px]'> : تعداد یادداشت</span>
+          </div>
+      </div>
+    </div>
+  )
+}
